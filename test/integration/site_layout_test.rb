e9f787f7c062afe0e7d82a1023e0a7620b5e2b3f
xuzhi <xz0618@126.com>
2015-11-13 13:04:35 +0800

    Change layout and routes

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -8,10 +8,14 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "layout links" do
     get root_path;
     assert_template 'static_pages/home'
+    # 这是 assert_select 方法的一些高级的用法,同时指定标签  a和属性  href  检查有没有指定链接
     assert_select "a[href=?]",root_path, count:2
     assert_select "a[href=?]",root_path
     assert_select "a[href=?]",help_path
     assert_select "a[href=?]",about_path
     assert_select "a[href=?]",contact_path
+    
+    get signup_path
+    assert_select "title",full_title("Sign up")
   end
 end
