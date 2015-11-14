49bd0f1ff80ea606bd0e982e6463c141c816cbd1
xuzhi <xz0618@126.com>
2015-11-14 16:53:13 +0800

    Found sign up pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+  def show 
+    @user = User.find(params[:id])
+  end
+
+  def create 
+    @user = User.new(user_params)#健壮参数的使用可以确定要选择接受的数据
+    #@user = User.new(params[:user])
+    # = @user = User.new(name: ...,email .....)
+    if @user.save
+      #Deal with the registered success!
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
 end
